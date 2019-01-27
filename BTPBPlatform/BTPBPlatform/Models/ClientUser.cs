4556264f548250c5f520b129a14075618d918333
David O'Neill Jr <doneill612@gmail.com>
2019-01-27 13:24:55 +0100

    minimize css, replace HTML with Razor where necessary, required fields in login page. TODO investigate session hooks for automatic logout.

--- BTPBPlatform/BTPBPlatform/Models/ClientUser.cs
+++ BTPBPlatform/BTPBPlatform/Models/ClientUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using BTPBCommon.Clients;
@@ -9,14 +10,41 @@ using Newtonsoft.Json;
 
 namespace BTPBPlatform.Models
 {
+    /// <summary>
+    /// Represents a platform user View-model 
+    /// </summary>
     public class ClientUser
     {
+        /// <summary>
+        /// The username of the of the user being signed in.
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// The full name of the user being signed in.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// The ID of the client the user being signed in belongs to.
+        /// </summary>
         public int ClientId { get; set; }
+
+        /// <summary>
+        /// The password of the client being signed in.
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// The user type of the client being signed in. 
+        /// </summary>
         public string UserType { get; set; }
 
+        /// <summary>
+        /// Authenticates this view model with the credentials obtained from the database.
+        /// </summary>
+        /// <param name="user">The <see cref="BTPBCommon.Clients.User"/> object retrived from the databse.</param>
+        /// <returns>True if the credentials provided during sign-in match the credentials stored in the DB, false otherwise</returns>
         public bool Authenticate(User user)
         {
             if (this.ClientId != user.ClientId)
